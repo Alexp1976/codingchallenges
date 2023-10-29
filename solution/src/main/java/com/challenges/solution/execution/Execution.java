1cb27ad9a25e168225312450e27c680f6ad28435
Alexp1976 <rpdemo@gmail.com>
2023-10-29 17:54:54 -0300

    Add solution

--- solution/src/main/java/com/challenges/solution/execution/Execution.java
+++ solution/src/main/java/com/challenges/solution/execution/Execution.java
@@ -1,6 +1,12 @@
 package com.challenges.solution.execution;
 
+import com.challenges.solution.appleandorange.AppleAndOrange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface Execution {
+
+    static final Logger logger = LoggerFactory.getLogger(AppleAndOrange.class);
 
     public void execute();
 }
