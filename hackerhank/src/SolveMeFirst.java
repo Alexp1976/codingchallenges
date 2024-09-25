4ab8e319212df84c1b259408566d1fe3c1171964
Alexp1976 <rpdemo@gmail.com>
2024-09-25 17:16:19 +0200

    Solve me first

---
+++ hackerhank/src/SolveMeFirst.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class SolveMeFirst {
+
+    // https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int a = Math.abs(random.nextInt(100));
+        int b = Math.abs(random.nextInt(100));
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(solveMeFirst(a, b));
+    }
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+}
