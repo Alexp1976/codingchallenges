a9b91e57ffd1750b2c91eb53b73cf88938ddc1d6
Alexp1976 <rpdemo@gmail.com>
2024-09-25 17:36:14 +0200

    Compare the triplets

---
+++ hackerhank/src/CompareTheTriplets.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompareTheTriplets {
+
+    // https://www.hackerrank.com/challenges/compare-the-triplets/problem?isFullScreen=true
+
+    public static void main(String[] args) {
+
+        System.out.println(compareTriplets(List.of(5,6,7), List.of(3,6,10)));
+    }
+
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+
+        int scoreA = 0;
+        int scoreB = 0;
+
+        for (int index = 0; index < a.size(); index++) {
+            if (a.get(index) >b.get(index)) {
+                scoreA++;
+            } else if (a.get(index) < b.get(index)) {
+                scoreB++;
+            }
+        }
+
+        return List.of(scoreA, scoreB);
+    }
+}
