2861741a380575d5b758f78a808893ce1f634851
Alexp1976 <rpdemo@gmail.com>
2024-09-25 17:21:11 +0200

    Simple Array Sum

---
+++ hackerhank/src/SimpleArraySum.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class SimpleArraySum {
+
+    // https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+
+    public static void main(String[] args) {
+
+        System.out.println(simpleArraySum(List.of(1,2,3,4,10,11)));
+    }
+
+    static int simpleArraySum(List<Integer> ar) {
+
+        int sum = 0;
+
+        for (int number : ar) {
+            sum += number;
+        }
+
+        return sum;
+    }
+}
